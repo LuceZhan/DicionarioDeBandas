e82ea22c78b213c00c19bf798d3b41f7bee98bd7
“[lucezhan]” <[nathalisboa@gmail.com]>
2025-04-23 20:56:29 -0300

    add foreach

--- DicionarioDeBandas/Program.cs
+++ DicionarioDeBandas/Program.cs
@@ -1,11 +1,15 @@
 ﻿string mensagemDeBoasVindas = "Boas vindas ao Screen Sound";
-void ExibirMensagensDeBoasVindas()
+List<string> listaBandas = new List<string>();
+
+void ExibirLogo()
 {
     Console.WriteLine(mensagemDeBoasVindas);
 
 }
 void ExibirOpcoesDoMenu()
+
 {
+    ExibirLogo();
     Console.WriteLine("\nDigite 1 para registrar uma banda.");
     Console.WriteLine("\nDigite 2 para mostrar todaas as bandas.");
     Console.WriteLine("\nDigite 3 para avaliar uma banda.");
@@ -18,22 +22,59 @@ void ExibirOpcoesDoMenu()
 
     switch (opcaoNumerica) 
     {
-        case 1: Console.WriteLine("Você digitou a opção " + opcaoEscolhida);
+        case 1: RegistrarBandas();
             break;
-        case 2:
-            Console.WriteLine("Você digitou a opção " + opcaoEscolhida);
+        case 2: MostrarBandas();
             break;
         case 3:
             Console.WriteLine("Você digitou a opção " + opcaoEscolhida);
             break;
         case 4:
-            Console.WriteLine("Você digitou a opção " + opcaoEscolhida);
+            Console.WriteLine("Bye-Bye :)");
             break;
         default: Console.WriteLine("Opção invalida");
             break;
     }
 
 }
+void RegistrarBandas()
+{
+    Console.Clear();
+    Console.WriteLine("Registro de bandas.");
+    Console.Write("Digite o nome da banda que deseja registrar: ");
+    string nomeDaBanda = Console.ReadLine()!;
+    listaBandas.Add(nomeDaBanda);
+    Console.WriteLine($"A banda {nomeDaBanda} foi registrada com sucesso!");
+    Thread.Sleep(2000);
+    Console.Clear();
+    ExibirOpcoesDoMenu();
+}
 
-ExibirMensagensDeBoasVindas();
+void MostrarBandas()
+{
+    Console.Clear();
+    Console.WriteLine("\nExibindo todas as bandas registradas!");
+   //for (int i = 0; i < listaBandas.Count; i++)
+    //{
+      //  Console.WriteLine($"Banda:: {listaBandas[i]}");
+    //}
+    foreach (string banda in listaBandas)
+    {
+        Console.WriteLine($"Banda: {banda}");
+    }
+    Console.WriteLine("Digite uma tecla para voltar ao menu.");
+    Console.ReadKey();
+    Console.Clear();
+    ExibirOpcoesDoMenu();
+}
+
+
+
+
+
+
+
+
+ExibirLogo();
 ExibirOpcoesDoMenu();
+
